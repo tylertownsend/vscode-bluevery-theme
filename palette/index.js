62ce4089cacdffc0a7fae410e4b9b6b7f95a60f9
Tyler Townsend <townsend.ts@outlook.com>
2020-07-29 20:32:27 -0700

    Initial commit

---
+++ palette/index.js
@@ -0,0 +1,34 @@
+const colors = {
+  // Black and White
+  black: "#1b1f23",
+  white: "#ffffff",
+  
+  // Traditional Colors
+  red: "#ea4a5a", //0
+  orange: "#fb8532", //1
+  yellow: "#ffdf5d", //2
+  green: "#34d058", //3
+  cyan: "#17E5E6", //4
+  blue: "#2188ff", //5
+  purple: "#8a63d2", //6
+  magnenta: "#ec6cb9",//7
+
+  // Accent Colors
+  accent0: "#03f0fc",
+  accent1: "#fc0373",
+  accent2: "#03f0fc",
+  accent3: "#fc0373",
+
+  // Background to Foreground Spectrum
+  // Sort from darkest to brightest
+  shade0: '#282b5e',
+  shade1: '#444775',
+  shade2: '#60628b',
+  shade3: '#7c7ea2',
+  shade4: '#9799b8',
+  shade5: '#b3b5cf',
+  shade6: '#cfd0e5',
+  shade7: '#ebecfc'
+}
+
+module.exports = colors;
